aadc966bcde1f370182dfd59a6f52cda1c2d6984
Kuba <kuba.lobacz@gmail.com>
2026-03-03 15:20:06 +0100

    Create project

---
+++ BookViewerApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BookViewerApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
